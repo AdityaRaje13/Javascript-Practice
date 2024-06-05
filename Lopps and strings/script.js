0c204657ece7a7c141ceee9829bafd50ee7d8987
Aditya Raje <138199963+AdityaRaje13@users.noreply.github.com>
2024-06-05 17:10:17 +0530

    Add files via upload

---
+++ Lopps and strings/script.js
@@ -0,0 +1,63 @@
+//For loop :
+// let n = prompt("Enter n value to calculate sum : ");
+// let res = 0;
+
+// for(let i = 1; i <= n; i++)
+// {
+//     res = res + i;
+// }
+
+// console.log("Addition from 1 to ",n ,"is ",res );
+
+
+//While loop :
+// let n = prompt("Enter n value to calculate sum : ");
+// let res = 0;
+// let i = 1;
+
+// while(i <= n)
+// {
+//     res = res + i;
+//     i++;
+// }
+
+// console.log("Addition from 1 to ",n ,"is ",res );
+
+//Do-while loop : 
+// let n = prompt("Enter n value to calculate sum : ");
+// let res = 0;
+// let i = 1;
+
+// do
+// {
+//     res = res + i;
+//     i++;
+// }
+// while(i <= n)
+
+// console.log("Addition from 1 to ",n ,"is ",res );
+
+//For-of Loop :
+// let str = "AdityaRaje";
+
+// for(let i of str)
+// {
+//     console.log(i);
+// }
+
+//For-in loop :
+
+// let student = {
+
+//     name : 'Aditya',
+//     age : 16,
+//     cgpa : 8.9,
+//     num : 144
+// };
+
+// for(let key in student)
+// {
+//     console.log("key = ",key, "value = ",student[key]);
+// }
+
+
